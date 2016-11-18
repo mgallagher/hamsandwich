9551316bfa5fac981ede2b5d3d4df233f65ec810
Christopher Reynolds <chris@pxp200.com>
2016-11-18 14:35:59 -0700

    - Update serializers to minimize what User fields show

--- entries/serializers.py
+++ entries/serializers.py
@@ -1,9 +1,17 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import Project, Entry
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'date_joined')
+
 
 class ProjectSerializer(serializers.ModelSerializer):
+    manager = UserSerializer()
+    members = UserSerializer(many=True)
     number_of_entries = serializers.SerializerMethodField()
 
     def get_number_of_entries(self, project):
